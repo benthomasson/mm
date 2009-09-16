18e04d45fe34cb86a56858298008fe83a7dd8188
Ben Thomasson <ben.thomasson@gmail.com>
2009-09-16 13:40:10 -0400

    Adding mountains

--- rooms.py
+++ rooms.py
@@ -9,10 +9,12 @@ class Grass(Updatable,Room):
     ticksPerTurn = 1000
     description = "tall green grass"
     detail = "green blades waving in the wind"
+    mapCharacter = "."
+    mapColor = "{LIGHTGREEN}"
+    lifetime = 1000
 
     def __init__(self,id=None):
         Room.__init__(self,id)
-        self.lifetime = 10
 
     def update(self,tick):
         self.lifetime += -1
@@ -25,10 +27,32 @@ class Thorns(Updatable,Room):
 
     description = "tall impassible thorns"
     detail = "sharp spines upon tangled vines"
+    mapCharacter = "x"
+    mapColor = "{GREEN}"
 
     def __init__(self,id=None):
         Room.__init__(self,id)
 
-    def checkHold(self,o):
+    def checkEnter(self,o):
         raise GameException("Thorns are impassible")
+
+
+class Mountains(Updatable,Room):
+
+    description = "tall impassible mountains"
+    detail = "sheer craggy cliffs"
+    mapCharacter = "^"
+    mapColor = "{WHITE}"
+
+    def __init__(self,id=None):
+        Room.__init__(self,id)
+
+    def checkEnter(self,o):
+        raise GameException("Mountains are impassible")
+
+roomMap = {
+    '.':Grass,
+    'x':Thorns,
+    '^':Mountains,
+}
 
