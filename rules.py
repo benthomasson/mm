fabfe98efa447714f697bc3475d2d916fec773b7
Ben Thomasson <ben.thomasson@gmail.com>
2010-01-01 21:55:33 -0500

    Adding condition factory

--- rules.py
+++ rules.py
@@ -1,16 +1,21 @@
 
 from pymud.rule import Rule, Pass, Fail, StopException, Action
 
-def LifeZero(self,o):
-    return o.life == 0
+def attributeEquals(attribute,value):
+    def _p(self,o):
+        return getattr(o,attribute) == value
+    return _p
+
+LifeZero = attributeEquals('life',0)
 
 def Delete(self,o):
     o.sendMessage('notice',notice='You died')
     o.sendLocationMessage('notice',notice='%s died' % o.name,exclude=o)
-    return o.delete()
+    o.delete()
+    raise StopException()
 
 Death = Rule(LifeZero,Delete)
 
 rules = {
-    '0_Death': Death
+    '0_Death': Death,
 }
